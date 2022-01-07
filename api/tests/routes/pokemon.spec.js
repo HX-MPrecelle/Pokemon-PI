85583ddcc2e0fe82b8ff82f4c9f392cabfdeddcb
Martin Precelle <tinchoprecelle@gmail.com>
2022-01-06 21:13:09 -0300

    Corrección de test para el pedido a la API

--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -17,8 +17,9 @@ describe('Pokemon routes', () => {
   beforeEach(() => Pokemon.sync({ force: true })
     .then(() => Pokemon.create(pokemon)));
   describe('GET /pokemons', () => {
-    it('should get 200', () =>
+    it('should get 200', (done) => {
       agent.get('/pokemons').expect(200)
-    );
+      done();
+    }).timeout(5000);
   });
 });
